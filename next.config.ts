2b0b26c92446ff2ad547b51208f557e4045072c8
eng2mary4 <eng2mary4@gmail.com>
2026-04-13 13:21:32 +0300

    next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,22 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // تحويل المشروع إلى static files
   output: "export",
+
+  // المسار الي رفعتِ فيه المشروع على cPanel
+  basePath: "/support2",
+
+  // يحل مشكلة /ar تجي تلقائي
+  i18n: undefined,
+
+  // مهم للصور تشتغل بدون مشاكل
   images: {
     unoptimized: true,
   },
+
+  // يمنع trailing slash يسبب مشاكل
+  trailingSlash: true,
 };
 
 export default nextConfig;
